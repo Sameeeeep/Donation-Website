30cb11dcb2cb6a993bde504cea32fce339e5f165
Kunal Patil <kp327313@gmail.com>
2022-07-21 22:18:40 +0530

    added hamburger

---
+++ script.js
@@ -0,0 +1,17 @@
+// let x = document.querySelector(".menu");
+// function open(){
+//    alert("Kunal!");
+//    x.display="none";
+
+// }
+const navItem = document.querySelector('.menu');
+function fast() {
+   console.log(navItem);
+   document.querySelector("#lists").style.display = "flex";
+}
+function closeNav() {
+   document.querySelector("#lists").style.display = "none";
+}
+const close  = document.querySelector("#close");
+close.addEventListener("click",closeNav)
+navItem.addEventListener("click", fast)
